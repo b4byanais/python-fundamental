3af88b666d13e2517ca15f3d5fd2343667419229
b4byanais <4l4n.d3f1t@gmail.com>
2021-10-17 10:39:28 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Sekuensial
+print('Ibu berkata, "Tolong belikan 1 botol susu"')
+print('"Jika ada telur, beli 6 telur"')
+print('Anak berkata, "OK"')
+print("Anak pergi ke toko")
+
+# Percabangan
+print("Berapa banyak susu yang ada di toko?")
+susu = int(input())
+if  susu > 0:
+    print("Anak membeli 1 botol susu")
+else:
+    print("Budi pulang ke rumah")
+print("Berapa banyak telur yang ada d toko?")
+telur = int(input())
+if  telur > 0:
+    print("Budi membeli 1 botol susu dan 6 buah telur")
+else:
+    print("Budi pulang ke rumah")
+print("Budi pulang ke rumah")
